c87528271aefba512d76518a6e9665e832ec2b7e
Connie Leung <connie.leung@diginex.com>
2021-05-27 15:17:58 +0800

    chore(prettier): update prettier config
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as helmet from 'helmet';
-import * as compression from 'compression';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as helmet from 'helmet'
+import * as compression from 'compression'
 import * as express from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true })
   app.use(express.json({ limit: '50mb' }))
   app.use(express.urlencoded({ limit: '50mb', extended: true }))
   app.use(helmet())
   app.use(compression())
-  await app.listen(4000);
+  await app.listen(4000)
 }
-bootstrap();
+bootstrap()
