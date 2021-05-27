73d8b6d5cf614dae4687ac10f5d0f95cf751aabb
Connie Leung <connie.leung@diginex.com>
2021-05-27 11:06:07 +0800

    chore: use express to parse json and urlencoded bodies
    Signed-off-by: Connie Leung <connie.leung@diginex.com>

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 import * as compression from 'compression';
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ limit: '50mb', extended: true }))
   app.use(helmet())
   app.use(compression())
   await app.listen(8000);
